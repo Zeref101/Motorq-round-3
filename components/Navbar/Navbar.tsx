2248858e96b93f7a0854151a0298755944d6ef61
Zeref101 <shreyas.mohanty03@gmail.com>
2024-08-12 22:18:15 +0530

    feat: Add Navbar and LeftSidebar components

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+// import Link from "next/link";
+// import location_marker from "public/icons/location_marker.svg";
+
+const Navbar = () => {
+    return (
+        <nav className='flex-between fixed z-50 w-[95%] bg-[#22283a] p-6 shadow-light-300 dark:shadow-none sm:px-12 rounded-md'>
+            <div className='flex gap-4 justify-center items-center'>
+                <Image src={"/public/icons/location_marker.svg"} alt='marker' width={20} height={20} />
+                <span className='text-white h2-bold'>FleetFusion</span>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ components/Sidebar/LeftSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeftSidebar = () => {
+    return (
+        <div className=' w-[80px] h-[85%] bg-[#22283a] rounded-md fixed z-50'>
+
+        </div>
+    )
+}
+
+export default LeftSidebar
