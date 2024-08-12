c84d62f38a3091603e82d0fe6e00c260b16bb570
Zeref101 <shreyas.mohanty03@gmail.com>
2024-08-12 22:17:59 +0530

    chore: Add layout component for main content

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,25 @@
+import Navbar from '@/components/Navbar/Navbar';
+import LeftSidebar from '@/components/Sidebar/LeftSidebar';
+import React from 'react'
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <main className="background-light850_dark100 relative">
+            <div className=' mx-8 pt-2'>
+                <Navbar />
+            </div>
+            <div className="flex">
+                <div className=' pl-8 pt-24'>
+
+                    <LeftSidebar />
+                </div>
+                <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+                    <div className="mx-auto w-full max-w-5xl">{children}</div>
+                </section>
+
+            </div>
+        </main>
+    );
+}
+
+export default layout
