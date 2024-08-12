c619299badd634716c86ffa67ac8ad32d3b547d8
Zeref101 <shreyas.mohanty03@gmail.com>
2024-08-12 22:18:29 +0530

    chore: Add working hours field to driver model

--- database/driver.model.ts
+++ database/driver.model.ts
@@ -1,3 +1,4 @@
+import { IWorkingHours } from "@/types";
 import { Schema, models, model, Document } from "mongoose";
 
 interface IDriver extends Document {
@@ -5,13 +6,38 @@ interface IDriver extends Document {
   email: string;
   phone_number: number;
   location?: string;
+  assigned_vehicle: Schema.Types.ObjectId;
+  working_hours: IWorkingHours[];
 }
+const WorkingHoursSchema = new Schema<IWorkingHours>({
+  day: {
+    type: String,
+    required: true,
+    enum: [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ],
+  },
+  start: { type: String, required: true },
+  end: { type: String, required: true },
+});
 
 const DriverSchema = new Schema<IDriver>({
   name: { type: String, required: true },
   email: { type: String, required: true },
   phone_number: { type: Number, required: true },
   location: { type: String, required: false },
+  assigned_vehicle: {
+    type: Schema.Types.ObjectId,
+    ref: "Vehicle",
+    required: false,
+  },
+  working_hours: { type: [WorkingHoursSchema], required: false },
 });
 
 const Driver = models.Driver || model("Driver", DriverSchema);
--- database/vehicle.model.ts
+++ database/vehicle.model.ts
@@ -4,12 +4,18 @@ interface IVehicle extends Document {
   brand: string;
   model_name: string;
   licensePlate: string;
+  fuel: string;
+  available: boolean;
+  transmission: string;
 }
 
 const VehicleSchema = new Schema<IVehicle>({
   brand: { type: String, required: true },
   model_name: { type: String, required: true },
   licensePlate: { type: String, required: true },
+  fuel: { type: String, required: false },
+  available: { type: Boolean, required: false },
+  transmission: { type: String, required: false },
 });
 
 const Vehicle = models.Vehicle || model("Vehicle", VehicleSchema);
