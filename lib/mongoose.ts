c1c3b4e0b56b562cbc80d8fb0a0d162c0c61595e
Zeref101 <shreyas.mohanty03@gmail.com>
2024-08-13 08:32:28 +0530

    refactor: Update mongoose connection to use MongoDB Atlas URI
    This commit updates the mongoose connection in the `mongoose.ts` file to use the MongoDB Atlas URI instead of the `process.env.MONGODB_URI` environment variable. This change ensures that the application connects to the correct MongoDB database and allows for easier configuration and deployment.

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
 
 export const connectionToDatabase = async () => {
-  if (!process.env.MONGODB_URI) {
-    return console.log("MISSING MONGODB_URI");
-  }
+  // if (!process.env.MONGODB_URI) {
+  //   return console.log("MISSING MONGODB_URI");
+  // }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "MotorQ-Assignment",
-    });
+    await mongoose.connect(
+      "mongodb+srv://Shreyas:thedarkslayer@cluster0.xkioikl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      {
+        dbName: "MotorQ-Assignment",
+      }
+    );
     console.log("Connected to MongoDB successfully!");
   } catch (error) {
     console.log(error);
